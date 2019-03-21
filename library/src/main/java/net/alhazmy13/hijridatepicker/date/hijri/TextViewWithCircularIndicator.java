4e65aaa033608bf61f8650ac3e026450ff46b12f
humazed <humazed@gmail.com>
2019-03-21 13:51:07 +0200

    migrate to androidx

--- library/src/main/java/net/alhazmy13/hijridatepicker/date/hijri/TextViewWithCircularIndicator.java
+++ library/src/main/java/net/alhazmy13/hijridatepicker/date/hijri/TextViewWithCircularIndicator.java
@@ -23,8 +23,8 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Paint.Align;
 import android.graphics.Paint.Style;
-import android.support.annotation.NonNull;
-import android.support.v4.content.ContextCompat;
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
--- sample/src/main/java/net/alhazmy13/hijridatepickerexample/TimePickerFragment.java
+++ sample/src/main/java/net/alhazmy13/hijridatepickerexample/TimePickerFragment.java
@@ -3,7 +3,7 @@ package net.alhazmy13.hijridatepickerexample;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import androidx.fragment.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
